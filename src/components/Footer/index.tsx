99f9eb44c84fea9b1d7b533f54c34cb43fdbbf5e
tunogya <tunogya@qq.com>
2022-06-19 22:27:03 +0800

    add twitter

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,16 @@
 import {Link, Spacer, Stack, Text, Wrap, WrapItem} from "@chakra-ui/react";
-import {FaGithub, FaDiscord} from "react-icons/all";
+import {FaGithub, FaDiscord, FaTwitter} from "react-icons/all";
 
 export const Footer = () => {
   const links = [
     { id: 0, icon: <FaDiscord size={24} /> , link: "https://discord.gg/hddy3D2ufY"},
     { id: 1, icon: <FaGithub size={24} />, link: "https://github.com/wakandalabs/geohash" },
+    { id: 2, icon: <FaTwitter size={24} />, link: "https://github.com/wakandalabs" }
   ]
 
   return (
     <Stack p={2} h={'full'}>
-      <Wrap>
+      <Wrap spacing={4}>
         { links.map((item) => (
           <WrapItem key={item.id}>
             <Link href={item.link} isExternal>
