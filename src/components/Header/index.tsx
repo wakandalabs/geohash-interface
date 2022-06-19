0275f62f8558141cc033291b0be643da0a02027e
tunogya <tunogya@qq.com>
2022-06-20 00:15:45 +0800

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import {HStack, IconButton, Spacer, Text} from "@chakra-ui/react";
 import {useLocation, useNavigate} from "react-router-dom";
-import {FaRegUserCircle, FiShare} from "react-icons/all";
+import {FaRegUserCircle, FiCornerUpLeft, FiShare} from "react-icons/all";
 
 export const Header = () => {
   const navigate = useNavigate()
@@ -9,7 +9,12 @@ export const Header = () => {
   return (
     <HStack alignItems={"center"} p={2}>
       <Text fontWeight={"semibold"} onClick={() => navigate('/') } cursor={"pointer"}>Geohash</Text>
-      <Text fontSize={'sm'}>{ pathname }</Text>
+      { pathname !== '/' && (
+        <>
+          <IconButton aria-label={''} icon={<FiCornerUpLeft />} variant={"ghost"} borderRadius={8} size={'sm'} onClick={() => navigate(pathname.slice(0, -1) ?? '/')}/>
+          <Text fontSize={'sm'}>{ pathname }</Text>
+        </>
+      ) }
       <Spacer />
       <IconButton aria-label={''} icon={<FiShare />} variant={"ghost"} borderRadius={8} size={'sm'} />
       <IconButton aria-label={''} icon={<FaRegUserCircle />} variant={"ghost"} borderRadius={8} size={'sm'}/>
