0d4c08b3d3b7adb3b50b33354ac75af66374b6aa
tunogya <tunogya@qq.com>
2022-06-20 23:39:54 +0800

    add abi

--- src/pages/Geohash/GeohashItem.tsx
+++ src/pages/Geohash/GeohashItem.tsx
@@ -1,15 +1,37 @@
-import {FC} from "react";
+import {FC, useCallback, useEffect, useState} from "react";
 import {Stack, Text} from "@chakra-ui/react";
 import {useLocation, useNavigate} from "react-router-dom";
+import {useGeohash} from "../../hooks/useGeohash";
+import {useGeohashContract} from "../../hooks/useContract";
+import {BigNumber} from "ethers";
 
-const GeohashItem: FC<{ alphabet: string, pathname: string }> = ({alphabet}) => {
+const GeohashItem: FC<{ pathname: string, alphabet: string}> = ({alphabet}) => {
   const navigate = useNavigate()
   const {pathname} = useLocation()
+  const geohash = useGeohashContract()
+  const [tokenId, setTokenId] = useState<string | undefined>(undefined)
+
+  const fetchTokenId = useCallback(async () => {
+    if (!geohash) return
+    try{
+      const result = await geohash.tokenByURI(`${pathname.slice(1)}${alphabet}`)
+      if (result) {
+        setTokenId(BigNumber.from(result).toString())
+      }
+    } catch (e) {
+      setTokenId(undefined)
+    }
+  }, [geohash, pathname, alphabet])
+
+  useEffect(() => {
+    fetchTokenId()
+  }, [fetchTokenId])
 
   return (
     <Stack bg='white' height='200px' boxShadow={'xs'} alignItems={"center"} justifyContent={"center"} cursor={"pointer"}
            onClick={() => navigate(`${pathname}${alphabet}`)}>
       <Text>{pathname.slice(1)}{alphabet}</Text>
+      <Text>{tokenId?.slice(0,4)}...{tokenId?.slice(-4)}</Text>
     </Stack>
   )
 }
