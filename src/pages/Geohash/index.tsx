b113bd23ead03abe2f3967969efaed1bb5640449
tunogya <tunogya@qq.com>
2022-06-22 22:49:46 +0800

    update

--- src/pages/Geohash/index.tsx
+++ src/pages/Geohash/index.tsx
@@ -2,14 +2,12 @@ import {useGeohash} from "../../hooks/useGeohash";
 import {Code, Stack} from "@chakra-ui/react";
 
 export const Geohash = () => {
-  const { totalSupply, allTokenIds, myBalance, myTokenIds } = useGeohash()
+  const { totalSupply, allTokenIds } = useGeohash()
 
   return (
     <Stack p={4}>
       <Code p={2}>total supply: {totalSupply}</Code>
-      <Code p={2}>All tokenIds: {allTokenIds}</Code>
-      <Code p={2}>my balance: {myBalance}</Code>
-      <Code p={2}>My tokenIds: {myTokenIds}</Code>
+      <Code p={2}>All tokenIds: {allTokenIds.length}</Code>
     </Stack>
   )
 }
